3b208fc7ee9e25a8d846590533320e812e425144
faimin <fuxianchao@gmail.com>
2020-10-26 18:20:43 +0800

    modify reference

--- Source/ZDFfiFunctions.h
+++ Source/ZDFfiFunctions.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <ffi.h>
+#import <libffi-core/ffi.h>
 #import "ZDFfiDefine.h"
 
 NS_ASSUME_NONNULL_BEGIN
--- Source/ZDFfiHookInfo.h
+++ Source/ZDFfiHookInfo.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
-#import <ffi.h>
+#import <libffi-core/ffi.h>
 #import "ZDFfiDefine.h"
 
 NS_ASSUME_NONNULL_BEGIN
