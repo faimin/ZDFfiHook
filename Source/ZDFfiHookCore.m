9ef7056a9edb001da66d3696ddbb7c002bda6489
faimin <fuxianchao@gmail.com>
2022-08-11 15:29:49 +0800

    arg count

--- Source/ZDFfiHookCore.m
+++ Source/ZDFfiHookCore.m
@@ -104,9 +104,11 @@ static void ZD_ffi_closure_func(ffi_cif *cif, void *ret, void **args, void *user
 #if DEBUG
     const char *mapedTypeEncoding = ZDFfi_ReduceBlockSignatureCodingType(info.typeEncoding).UTF8String;
     NSUInteger argCount = 0;
-    while (mapedTypeEncoding[argCount]) {
-        ++argCount;
-    };
+    for (NSInteger i = 0; mapedTypeEncoding[i] != '\0'; ++i) {
+        if (mapedTypeEncoding[i] == ':') {
+            ++argCount;
+        }
+    }
     printf("参数个数：-------- %zd\n", argCount);
     printf("精简后的方法签名：%s\n", mapedTypeEncoding);
     
